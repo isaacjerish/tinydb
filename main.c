555eebba7f77cbee87e6e6f540bdbb5f423761a2
Isaac Jerish <152412476+isaacjerish@users.noreply.github.com>
2025-05-01 01:57:29 -0400

    Started insert and select commands

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
-/* tinydb/main.c – Milestone 0: bootstrap REPL */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
 #define INPUT_BUFFER_SIZE 256
-
+typedef enum {
+    INSERT,
+    SELECT,
+    IDK
+} Statement;
+Statement parser(const char* input) {
+    if(strncmp(input, "insert", 6) == 0) {return INSERT;}
+    else if (strncmp(input, "select", 6) == 0) {return SELECT;}
+    else {return IDK;}
+}
 int main(void)
 {
     char input[INPUT_BUFFER_SIZE];
@@ -16,14 +24,25 @@ int main(void)
             break;
         }
         size_t len = strlen(input);
-        if (len && input[len - 1] == '\n')
+        if ((len!=0) && (input[len - 1] == '\n'))
             input[len - 1] = '\0';
         if (input[0] == '.') {
             if (strcmp(input, ".exit") == 0) {
                 puts("Bye!");
                 break;
             }
-            printf("Unrecognized meta-command '%s'\n", input);
+            else if (strcmp(input, ".help") == 0) {
+                printf("Available commands: \n .exit    Exit the data base \n .help    Show this help message\n");
+                continue;
+            }
+        }
+        Statement curr = parser(input);
+        if (curr == INSERT) {
+            puts("Insert recogonized");
+            continue;
+        }
+        else if (curr == SELECT) {
+            puts("Select recogonized");
             continue;
         }
         printf("Unrecognized keyword at start of '%s'\n", input);
